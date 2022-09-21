6a4a97bea7f38aa7adc0ac3e241c9495783b122b
sobop <88311796+sobop@users.noreply.github.com>
2022-09-21 21:30:49 +0900

    first commit
    Js -> Ts 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [hello, setHello] = useState('');
+  const [hello, setHello] = useState("");
 
   const fetchUsers = async () => {
-      const response = await axios.get(          '/hello'      );
-      setHello(response.data); // 데이터는 response.data 안에 들어있습니다.
+    const response = await axios.get("/hello");
+    setHello(response.data); // 데이터는 response.data 안에 들어있습니다.
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {hello}
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
