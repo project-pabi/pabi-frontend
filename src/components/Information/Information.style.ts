06ed122c87d036441e0b6a8f05161ce0469d92c5
sobop <88311796+sobop@users.noreply.github.com>
2022-10-03 23:43:54 +0900

    Information 버튼 기능 추가

--- src/components/Information/Information.style.ts
+++ src/components/Information/Information.style.ts
@@ -1,6 +1,48 @@
 import tw from "tailwind-styled-components";
 
-export const StyledContainer = tw.div`
-  max-w-7xl
-  bg-black
+export const Title = tw.div`
+  font-bold
+  text-[40px]
+  mb-[12px]
+  leading-[59px]
+`;
+export const SubTitle = tw.div`
+  text-[16px]
+  text-[#424242]
+  mb-[34px]
+`;
+export const TabTitle = tw.div`
+font-bold
+text-[24px]
+leading-[36px]
+`;
+export const RadioButton = tw.li`
+w-[240px]
+h-[50px]
+leading-[50px]
+bg-slate-400
+text-center
+rounded-[10px]
+`;
+export const TextBox = tw.input`
+w-[430px]
+h-[70px]
+bg-[#F5F5F5]
+rounded-[10px]
+p-6
+mt-[55px]
+`;
+export const NextButton = tw.div`
+w-[85px]
+h-[40px]
+bg-[#0000D8]
+text-[#FFFFFF]
+text-[14px]
+leading-[40px]
+rounded-[10px]
+cursor-pointer
+justify-center
+mx-auto
+mt-[50px]
+mb-[200px]
 `;
--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -1,3 +1,86 @@
-export default function Information() {
-  return <div className="container lg:mx-auto">상품정보 입력</div>;
-}
+import React, { useState } from "react";
+import {
+  NextButton,
+  SubTitle,
+  TabTitle,
+  TextBox,
+  Title,
+} from "./Information.style";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Box, Tab } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import styled from "styled-components";
+
+const Information = () => {
+  const [value, setValue] = useState("1");
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+  console.log(value);
+  const onIncrement = () => {
+    setValue(String(Number(value) + 1));
+  };
+  return (
+    <div className="container max-w-[1200px] m-auto pt-[70px]">
+      <Title>상품정보 입력</Title>
+      <SubTitle>비우고 싶은 제품이 무엇인지 설명해주세요!</SubTitle>
+      <TabContext value={value}>
+        <Box sx={{}}>
+          <TabList
+            onChange={handleChange}
+            aria-label="lab API tabs example"
+            className="mb-[107px]"
+          >
+            <StyledTab
+              label="제품 이름 입력"
+              value="1"
+              theme={createTheme({
+                palette: { primary: { main: "#0000D8" } },
+              })}
+            />
+            <StyledTab label="제품 종류 선택" value="2" />
+            <StyledTab label="제품 상태 선택" value="3" />
+            <StyledTab label="제품 사진 등록" value="4" />
+            <StyledTab label="제품 설명 입력" value="5" />
+          </TabList>
+        </Box>
+        <TabPanel value="1" className="text-center">
+          <TabTitle>비우려는 물건의 이름을 알려주세요</TabTitle>
+          <TextBox
+            type={"text"}
+            placeholder="물건의 이름이 무엇인가요?"
+          ></TextBox>
+          <NextButton onClick={onIncrement}>다음으로</NextButton>
+        </TabPanel>
+
+        <TabPanel value="2" className="text-center">
+          <TabTitle>물건의 종류는 무엇인가요?</TabTitle>
+          <NextButton onClick={onIncrement}>다음으로</NextButton>
+        </TabPanel>
+        <TabPanel value="3" className="text-center">
+          <TabTitle>물건의 상태는 어떤가요?</TabTitle>
+          <NextButton onClick={onIncrement}>다음으로</NextButton>
+        </TabPanel>
+        <TabPanel value="4" className="text-center">
+          <TabTitle>비우려는 물건의 모습을 보여주세요</TabTitle>
+          <NextButton onClick={onIncrement}>다음으로</NextButton>
+        </TabPanel>
+        <TabPanel value="5" className="text-center">
+          <TabTitle>비우려는 물건에 대해 하고싶은 말이 있나요?</TabTitle>
+        </TabPanel>
+      </TabContext>
+    </div>
+  );
+};
+export default Information;
+
+const StyledTab = styled(Tab)`
+  width: 240px;
+  height: 50px;
+  && {
+    background-color: #ffffff;
+    color: #616161;
+    font-size: 16px;
+    border-radius: 10px;
+  }
+`;
