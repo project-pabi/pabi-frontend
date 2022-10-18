6d2ae59398d8a5c6d0bab5f1ceb0b3540b7220d7
lsy7851 <76462754+lsy7851@users.noreply.github.com>
2022-10-18 14:27:05 +0900

    테일윈드, 스타일드 컴포넌트 통합 및 스타일 재정의 PW-48

---
+++ src/components/NewInformation/Information.style.ts
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Tab } from "@mui/material";
+
+export const Title = tw.div`
+  font-bold
+  text-[40px]
+  mb-[12px]
+  leading-[59px]
+`;
+export const SubTitle = tw.div`
+  text-[16px]
+  text-[#424242]
+  mb-[34px]
+`;
+export const TabTitle = tw.div`
+  text-[24px]
+  font-bold
+`;
+export const RadioButton = tw.li`
+w-[240px]
+h-[50px]
+leading-[50px]
+bg-slate-400
+text-center
+rounded-[10px]
+`;
+export const TextBox = tw.input`
+w-[430px]
+h-[70px]
+bg-[#F5F5F5]
+rounded-[10px]
+p-6
+mt-[55px]
+`;
+export const PrevButton = tw.div`
+w-[85px]
+h-[40px]
+text-[#0000D8]
+text-[14px]
+leading-[36px]
+rounded-[20px]
+cursor-pointer
+justify-center
+mr-5
+border-2 border-solid border-[#0000D8]
+`;
+export const NextButton = tw.div`
+w-[85px]
+h-[40px]
+bg-[#0000D8]
+text-[#FFFFFF]
+text-[14px]
+leading-[40px]
+rounded-[20px]
+cursor-pointer
+justify-center
+`;
+export const StyledTab = styled(Tab)`
+  ${tw`h-12`}
+  && {
+    ${tw`bg-white text-[#616161] text-[16px] rounded-[10px]`}
+  }
+`;
+export const Label = styled.label`
+  ${tw`
+    inline-block
+    cursor-pointer
+    bg-[#eee]
+    rounded-[20px]
+    border-2
+    border-white
+    text-[16px] 
+    text-[#424242]
+    h-11
+    px-2.5
+    py-4
+    leading-[20px]
+  `}
+`;
+export const Input = styled.input`
+  display: none;
+  &:checked + ${Label} {
+    ${tw`bg-white text-[#0000d8] border-2 border-[#0000d8] leading-[20px]`}
+  }
+`;
