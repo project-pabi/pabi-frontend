a80be56faa29efe6966b4400cb54481d66ae5d6a
lsy7851 <76462754+lsy7851@users.noreply.github.com>
2022-11-07 23:41:36 +0900

    [Feat] 폼 올바르지 않은 경로로 접근이 리다이렉트 PW-77

--- src/router/write/write.tsx
+++ src/router/write/write.tsx
@@ -1,13 +1,20 @@
-import { Route } from "react-router-dom";
-import Write from "@pages/Write/Write";
-import Porduct from "@component/NewInformation/InformationProduct";
-import Step2 from "@component/NewInformation/InformationCategory";
-import Result from "@component/NewInformation/InformationResult";
+import { Route, Navigate } from 'react-router-dom';
+import Write from '@pages/Write/Write';
+import Porduct from '@component/NewInformation/InformationProduct';
+import Name from '@component/NewInformation/InformationName';
+import Category from '@component/NewInformation/InformationCategory';
+import Result from '@component/NewInformation/InformationResult';
 export default (
   <Route path="write/*" element={<Write />}>
-    <Route path="" element={<Porduct />}></Route>
-    <Route path="product/*" element={<Porduct />}></Route>
-    <Route path="auction/*" element={<Step2 />}></Route>
+    <Route path="product/*" element={<Porduct />}>
+      <Route path="name" element={<Name />}></Route>
+      <Route path="category" element={<Category />}></Route>
+      <Route path="*" element={<Navigate to="./name"></Navigate>}></Route>
+    </Route>
+    <Route path="auction/*" element={<Category />}>
+      <Route path="*" element={<Navigate to="./name"></Navigate>}></Route>
+    </Route>
     <Route path="result" element={<Result />} />
+    <Route path="*" element={<Navigate to="./product/name"></Navigate>}></Route>
   </Route>
 );
