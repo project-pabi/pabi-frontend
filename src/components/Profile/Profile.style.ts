05329ce13333b81718d918e2b72db1ee1b939691
sobop <88311796+sobop@users.noreply.github.com>
2022-11-15 04:28:19 +0900

    [Feat] 상대 프로필 화면 퍼블리싱 PW-83
    2차

--- src/components/Profile/Profile.style.ts
+++ src/components/Profile/Profile.style.ts
@@ -7,19 +7,26 @@ interface WProps {
 
 export const Container = tw.div`
 container max-w-[1200px] m-auto pt-8 pb-[88px]
+
 `;
 export const List = styled.div<WProps>`
   width: ${(props) => props.width};
+
   ${tw`
   flex h-[180px] rounded-[30px] mr-6 last:mr-0
   shadow-[0_4px_20px_4px_rgba(198, 198, 224, 0.8)]
-  `}
+  
+  `};
 `;
 export const Box = tw.div`
-flex justify-center mb-10 
+flex justify-center mb-10
+`;
+export const ImgBox = tw.div`
+flex mr-[10px]
+items-center
 `;
 export const Img = tw.img`
-w-[120px] h-[120px] rounded-[50%] mr-5
+object-cover rounded-[50%]
 `;
 export const CountBox = tw.div`
 w-[110px] h-[62px] text-center
@@ -30,4 +37,16 @@ font-bold text-[40px] text-[#000060]
 `;
 export const CountText = tw.div`
 text-xs text-[#757575] mt-6
+`;
+export const Review = tw.div`
+
+`;
+export const Nickname = tw.div`
+text-base text-[#424242]
+`;
+export const Rating = tw.div`
+text-lg text-[#0000D8]
+`;
+export const Comment = tw.div`
+text-base text-[#424242]
 `;
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,23 +1,53 @@
+import { useState } from "react";
 import {
   Box,
+  Comment,
   Container,
   Count,
   CountBox,
   CountText,
   Img,
+  ImgBox,
   List,
+  Nickname,
+  Rating,
+  Review,
 } from "./Profile.style";
 
 export default function Profile() {
+  const [review, setReview] = useState([
+    {
+      src: "https://dimg.donga.com/a/500/0/90/5/ugc/CDB/29STREET/Article/5f/10/fd/c1/5f10fdc11b4dd273825d.jpg",
+      nickname: "여행가는 핑구",
+      rating: 2,
+      comment:
+        "아주 좋아요~ 아주 좋아요~ 아주 좋아요~ 아주 좋아요~ 아주 좋아요~ 아주 좋아요~ 아주 좋아요~ ",
+    },
+    {
+      src: "https://mblogthumb-phinf.pstatic.net/MjAxOTEwMjdfMzUg/MDAxNTcyMTU5MDAxMzI5.W1GIPQd0z73GxQ78Pj31C9KD3fC4aYQLNRMEX_AKp3Mg.oxeTqHTkh4-i2sG_tz3C11qzHCIYNgkVP34UkTXsU7Ug.JPEG.msjin93/IMG_8495.JPG?type=w800",
+      nickname: "늙은 핑구",
+      rating: 4,
+      comment: "허허 좋구만",
+    },
+    {
+      src: "https://mblogthumb-phinf.pstatic.net/MjAxOTEwMjdfMzUg/MDAxNTcyMTU5MDAxMzI5.W1GIPQd0z73GxQ78Pj31C9KD3fC4aYQLNRMEX_AKp3Mg.oxeTqHTkh4-i2sG_tz3C11qzHCIYNgkVP34UkTXsU7Ug.JPEG.msjin93/IMG_8495.JPG?type=w800",
+      nickname: "늙은 핑구",
+      rating: 4,
+      comment: "허허 좋구만",
+    },
+  ]);
   return (
     <Container>
       <Box>
         <List width="310px" className="p-7">
           <Img
+            className="w-[120px] h-[120px] mr-5"
             src="https://mblogthumb-phinf.pstatic.net/MjAxOTEwMjdfMjk3/MDAxNTcyMTU4OTA1NjAz.zHEicJ1aBtmkKS4bRYy02y_fBcbvLrWbFTcbUeUBnvIg.knnGDJUVIz4TcrVN7ARyAtfel9_JlbYRn1t2VUFjNtIg.JPEG.msjin93/IMG_8470.JPG?type=w800"
             alt="pinggu"
           />
-          <div className="text-2xl mt-6 font-bold text-[#000060]">핑구12</div>
+          <div className="text-2xl mt-6 font-bold text-[#000060]">
+            생각하는 핑구
+          </div>
         </List>
         <List width="420px" className="pt-[58px] px-[42px]">
           <CountBox>
@@ -42,10 +72,54 @@ export default function Profile() {
           </div>
         </List>
       </Box>
+
       <Box>
-        <List width="310px"></List>
-        <List width="420px"></List>
-        <List width="530px"></List>
+        <List width="310px" className="p-6">
+          <ImgBox>
+            <Img
+              className="w-[90px] h-[90px]"
+              src="https://www.pngmart.com/files/12/Instagram-Verified-Badge-PNG-Transparent-Image.png"
+              alt="badge"
+            />
+          </ImgBox>
+          <div className="w-[157px]">
+            <div className="text-base">관리자 인증</div>
+            <div className="text-sm mt-[14px]">
+              와! 관리자인증! 와! 관리자인증! 와! 관리자인증! 와! 관리자인증!
+              와! 관리자인증! 와! 관리자인증!
+            </div>
+          </div>
+        </List>
+        <List width="198px"></List>
+        {review.map((e: any, i: any) => (
+          <List width="309px" className="py-6 px-5" key={i}>
+            <Img className="w-12 h-12 mr-2" src={e.src} alt="pinggu" />
+            <Review>
+              <Nickname>{e.nickname}</Nickname>
+              <Rating>
+                {e.rating} {"🧊".repeat(e.rating)}
+              </Rating>
+              <Comment>{e.comment}</Comment>
+            </Review>
+          </List>
+        ))}
+      </Box>
+      <Box>
+        <List width="309px" className="py-6 px-5">
+          <Img
+            className="w-12 h-12 mr-2"
+            src="https://mblogthumb-phinf.pstatic.net/MjAxOTEwMjdfMjk3/MDAxNTcyMTU4OTA1NjAz.zHEicJ1aBtmkKS4bRYy02y_fBcbvLrWbFTcbUeUBnvIg.knnGDJUVIz4TcrVN7ARyAtfel9_JlbYRn1t2VUFjNtIg.JPEG.msjin93/IMG_8470.JPG?type=w800"
+            alt="pinggu"
+          />
+          <Review>
+            <Nickname>뱁세</Nickname>
+            <Rating>5</Rating>
+            <Comment>
+              몇 일 동안 찜해놓고 노리고 있었는데, 장 열자마자 바로 낙찰!! 싸게
+              낚아서 쥬아요~
+            </Comment>
+          </Review>
+        </List>
       </Box>
     </Container>
   );
