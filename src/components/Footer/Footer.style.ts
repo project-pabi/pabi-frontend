4085679e4514b25684b8fc46a6e9906c744c04be
sobop <lyn981026@gmail.com>
2022-12-07 23:24:02 +0900

    [Feat] footer 수정 PW-120
    1차 수정

---
+++ src/components/Footer/Footer.style.ts
@@ -0,0 +1,11 @@
+import tw from "twin.macro";
+export const MainBox = tw.div`
+flex items-center justify-between
+`;
+export const PabiLogo = tw.img`
+  w-40 items-center 
+`;
+export const MainContent = tw.div`
+text-sm text-[#424242]
+ml-11 first:ml-0 first:text-primary
+`;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,26 @@
+import { MainBox, MainContent, PabiLogo } from "./Footer.style";
+import Logo from "./logo.svg";
+
 const Footer = () => {
   return (
-    <footer className="border-solid h-[100px] pt-8">
-      <div className="max-w-[1700px] m-auto text-[#757575] text-[12px] leading-[18px]">
-        <ul className="flex items-center justify-center">
-          <li className="mr-[20px] ">팀소개</li>
-          <li className="mr-[20px]">|</li>
-          <li>경매참여안내</li>
-        </ul>
-        <div className="text-center mt-[10px]">
-          COPYRIGHT ©2022TEAM.PABI ALL RIGHTS RESERVED.
+    <footer className="border-solid h-[100px] pt-8 max-w-[1400px] m-auto border-t-[1px]">
+      <MainBox>
+        <PabiLogo src={Logo} />
+
+        <div className="flex">
+          <MainContent>팀소개</MainContent>
+          <MainContent>서비스 이용약관</MainContent>
+          <MainContent>개인정보 취급방침</MainContent>
+          <MainContent>위치기반서비스 이용약관</MainContent>
         </div>
+      </MainBox>
+      <div className="mt-[30px] text-xs text-[#212121]">TEAM YEOWOO-BI</div>
+      <div className="mt-5 text-xs text-[#212121]">
+        주소: 경기도 성남시 중원구 갈마치로 288번길 14, SKV1타워 A동 1003호
+      </div>
+      <div className="mt-5 text-xs text-[#212121]">MAIL: pa-bi@pa-bi.com</div>
+      <div className="mt-[38px] text-xs text-[#212121] float-right mb-10">
+        Copyright ©2022 파비 Pa-Bi All rights reserved.
       </div>
     </footer>
   );
