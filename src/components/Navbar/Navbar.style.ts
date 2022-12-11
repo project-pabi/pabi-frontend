1dfb1cf3023ae47c3370c94079aff74c900f95d9
sobop <lyn981026@gmail.com>
2022-12-11 23:57:42 +0900

    [Feat] header 수정 PW-121
    1차 수정

--- src/components/Navbar/Navbar.style.ts
+++ src/components/Navbar/Navbar.style.ts
@@ -1,16 +1,15 @@
 import tw from "twin.macro";
 
 export const NavContainer = tw.div`
-  flex
-  w-90%
-  mx-0
-  my-auto
-  h-full
+mt-[70px] max-w-[1440px] m-auto 
 `;
-
+export const SearchBox = tw.input`
+  border border-solid border-2 border-[#EEEEEE] w-[500px] h-12 rounded-[20px]
+  pl-6 pr-10 py-3 relative
+`;
 export const PabiLogo = tw.img`
   w-40 items-center 
 `;
-export const Menu = tw.li`
-  mr-10 last:mr-0 text-xl cursor-pointer
+export const Menu = tw.div`
+  mr-10 last:mr-0 cursor-pointer
 `;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,43 +1,39 @@
-import { Menu, NavContainer, PabiLogo } from "./Navbar.style";
+import { Menu, NavContainer, PabiLogo, SearchBox } from "./Navbar.style";
 import Logo from "./logo.svg";
 import { Link, useNavigate } from "react-router-dom";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Navbar = () => {
   let navigate = useNavigate();
   return (
-    <nav className="border-solid border-b border-[#D9D9D9]">
-      <div className="max-w-[1700px] m-auto  h-[80px]">
-        <NavContainer>
-          <div className="flex items-center">
-            <Link to="/">
-              <PabiLogo src={Logo} />
-            </Link>
+    <nav className="border-solid border-b border-[#D9D9D9] h-[200px]">
+      <NavContainer>
+        <div className="flex items-center justify-around border-solid pb-3 border-[#EEEEEE] border-b-[1px]">
+          <Link to="/">
+            <PabiLogo src={Logo} />
+          </Link>
+          <div className="relative">
+            <SearchBox placeholder="검색"></SearchBox>
+            <div className="absolute top-3 right-5">
+              <SearchIcon className="text-[#9E9E9E]" />
+            </div>
           </div>
-
-          <ul className="flex flex-1 items-center justify-center">
-            <Menu>인기매물</Menu>
-            <Menu>진행 중인 매물</Menu>
-            <Menu>임박매물</Menu>
-            <Menu>게시글 작성</Menu>
-          </ul>
-          <ul className="flex items-center justify-end">
-            <Menu
-              onClick={() => {
-                navigate("/signup");
-              }}
-            >
-              회원가입
+          <div className="flex">
+            <Menu>
+              <NotificationsNoneIcon className="text-primary" />
             </Menu>
-            <Menu
-              onClick={() => {
-                navigate("/signin");
-              }}
-            >
-              로그인
-            </Menu>
-          </ul>
-        </NavContainer>
-      </div>
+            <Menu>파비톡</Menu>
+            <Menu>내 물건 팔고 비우기</Menu>
+          </div>
+        </div>
+        <div className="flex items-center justify-around mt-7">
+          <div className="flex">
+            <Menu>실시간 경매</Menu>
+            <Menu>실시간 경매</Menu>
+          </div>
+        </div>
+      </NavContainer>
     </nav>
   );
 };
