410885bf7b468f0282e7643f79e98ec91e4d6cfc
이상엽 <yeopy.dev@gmail.com>
2023-03-23 00:06:02 +0900

    [Delete] 사용 안하는 파일 정리

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,28 +3,24 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFound from "../pages/NotFound/NotFound";
 import ProfilePage from "../pages/ProfilePage/ProfilePage";
 import RealtimePage from "../pages/RealTimePage/RealTimePage";
-import Select from "../pages/Select/Select";
 import SignEndPage from "../pages/SignEndPage/SignEndPage";
 import SignInPage from "../pages/SignInPage/SignInPage";
 import SignUpPage from "../pages/SignUpPage/SignUpPage";
 import Main from '../pages/Main';
-import Login from '../pages/Member/login';
 import { Write } from '@router/write';
+
 export default function index() {
   return (
     <Router>
       <Routes>
         <Route path="" element={<Main />}>
           {Write}
-          <Route path="select" element={<Select />} />
         </Route>
-        <Route path="select" element={<Select />} />
         <Route path="signin" element={<SignInPage />} />
         <Route path="signup" element={<SignUpPage />} />
         <Route path="signend" element={<SignEndPage />} />
         <Route path="profile" element={<ProfilePage />} />
         <Route path="realtime" element={<RealtimePage />} />
-        <Route path="login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
